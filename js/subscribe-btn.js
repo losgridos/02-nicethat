6b31456bf1987713975a6f5635b63d9496f8de18
Luis Caballero <mendoza386@gmail.com>
2020-05-07 21:16:38 +0200

    submit js functionality

--- js/subscribe-btn.js
+++ js/subscribe-btn.js
@@ -1,109 +1,87 @@
-function test() {
-  alert("Please, fill in this text");
-}
+let feedback = {
+  empty: function() {
+    alert("Please, fill in this text");
+  },
+  noAt: function() {
+    alert("please, include an @ in the email address");
+  },
+  noDot: function() {
+    alert("please, a dot is missing in the email address");
+  },
+  invalid: function() {
+    alert("Please provide a valid email address");
+  },
+  success: function() {
+    let submitBtns = document.querySelectorAll('.js-submit-btn');
+    let submitBtnsArray = Array.from(submitBtns);
 
-function at() {
-   alert("please, include an @ in the email address");
-}
+    submitBtnsArray.forEach(submitBtn => {
+      let success = document.querySelector(".success-feedback");
+      if (document.body.contains(success)) {
+        success.remove();
+      }
+      submitBtn.insertAdjacentHTML("afterend", "<p class=\"success-feedback\">Thank you for subscribing! you\'ll receive a confirmation email shortly</p>")
+    });
+  },
+  successRemove: function() {
+    let success = document.querySelector(".success-feedback");
+    if (document.body.contains(success)) {
+      success.remove();
+    }
+  }
+};
 
-function dot() {
-   alert("please, a dot is missing in the email address");
-}
 
-
-function invalid() {
-   alert("Please provide a valid email address");
-}
-
-function success() {
-  alert('Thank you for subscribing! you\'ll receive a confirmation email shortly');
-}
 
 
 function showFeedback(event) {
-  // 1
-
   if (!event.target.matches('.js-submit-btn')) return;
   event.preventDefault()
   let submitBtn = event.target.closest('.js-submit-btn');
-  // console.log(submitBtn);
+
+
 
   let inputs = document.querySelectorAll('.input-email');
-  // console.log(inputs);
-
   let inputsArray = Array.from(inputs);
-  // console.log(inputsArray);
 
-  // function validateEmail(email)
-  //   {
-  //       var re = /\S+@\S+\.\S+/;
-  //       re.test(email);
-  //   };
+  // let success = document.querySelector(".success-feedback");
 
   inputsArray.forEach(input => {
-    // console.log(input);
-    // console.log(input.value);
     let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     if (input.value == "") {
-      // console.log('Please, fill in this text');
-      //return false;
-      let emptyValue = false;
-      // return emptyValue;
-      // console.log(emptyValue);
-      test();
+      feedback.empty();
+      feedback.successRemove();
       return
     } else if (!input.value.includes('@')) {
-        at();
-        console.log('please, include an @ in the email address');
+        feedback.noAt()
+        feedback.successRemove();
         return false;
     } else if (!input.value.includes('.')) {
-        dot();
-        console.log('please, a dot is missing in the email address');
+        feedback.noDot();
+        feedback.successRemove();
         return false;
       }
 
       if (regex.test(input.value) == true) {
-      success();
-      console.log('Thank you for subscribing! you\'ll receive a confirmation email shortly')
+      feedback.success();
+      emailList.push(input.value);
+      emailList.push(midweek.checked);
+      console.log(emails);
+      // addEmail();
       return true;
     } else {
-      invalid();
-      console.log('Please provide a valid email address.');
-      return true;
+      feedback.invalid();
+      feedback.successRemove();
+      return false;
     }
   });
-
-  // atPosition = input.value.indexOf('@');
-
-
-  // atpos = emailID.indexOf("@");
-  //        dotpos = emailID.lastIndexOf(".");
-
-  //        if (atpos < 1 || ( dotpos - atpos < 2 )) {
-  //           alert("Please enter correct email ID")
-  //           document.myForm.EMail.focus() ;
-  //           return false;
-  //        }
-  // 2
-  // let arrayTags = Array.from(tagBtn.children);
-  // // 3
-  // arrayTags.forEach(tag => {
-  //   tagBtn.before(tag);
-  //   tag.classList.remove("u-hidden");
-  // });s
-  // // 4
-  // tagBtn.remove();
 }
 
+let emailList = [];
 
-// let at = '@';
-// let dot = '.';
 
-// let together = at.localeCompare(dot);
-// console.log(together);
 
 
 let parentElement = document.querySelector('body');
-
 parentElement.addEventListener('click', showFeedback);
