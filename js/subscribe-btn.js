3dfee8ef787f68a2a7404a38cead3436ca309b04
Luis Caballero <mendoza386@gmail.com>
2020-05-11 21:49:16 +0200

    newsletter functions finished

--- js/subscribe-btn.js
+++ js/subscribe-btn.js
@@ -1,54 +1,24 @@
-let feedback = {
-  empty: function() {
-    alert("Please, fill in this text");
-  },
-  noAt: function() {
-    alert("please, include an @ in the email address");
-  },
-  noDot: function() {
-    alert("please, a dot is missing in the email address");
-  },
-  invalid: function() {
-    alert("Please provide a valid email address");
-  },
-  success: function() {
-    let submitBtns = document.querySelectorAll('.js-submit-btn');
-    let submitBtnsArray = Array.from(submitBtns);
+// emailList store all the submissions
+let emailList = [];
 
-    submitBtnsArray.forEach(submitBtn => {
-      let success = document.querySelector(".success-feedback");
-      if (document.body.contains(success)) {
-        success.remove();
-      }
-      submitBtn.insertAdjacentHTML("afterend", "<p class=\"success-feedback\">Thank you for subscribing! you\'ll receive a confirmation email shortly</p>")
-    });
-  },
-  successRemove: function() {
-    let success = document.querySelector(".success-feedback");
-    if (document.body.contains(success)) {
-      success.remove();
-    }
-  }
+function subscribeBtn() {
+  let parentElement = document.querySelector('body');
+  parentElement.addEventListener('click', showFeedback);
 };
 
-
+subscribeBtn();
 
 
 function showFeedback(event) {
   if (!event.target.matches('.js-submit-btn')) return;
   event.preventDefault()
+
+  let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   let submitBtn = event.target.closest('.js-submit-btn');
-
-
-
   let inputs = document.querySelectorAll('.input-email');
   let inputsArray = Array.from(inputs);
 
-  // let success = document.querySelector(".success-feedback");
-
   inputsArray.forEach(input => {
-    let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
     if (input.value == "") {
       feedback.empty();
       feedback.successRemove();
@@ -61,27 +31,84 @@ function showFeedback(event) {
         feedback.noDot();
         feedback.successRemove();
         return false;
-      }
-
-      if (regex.test(input.value) == true) {
-      feedback.success();
-      emailList.push(input.value);
-      emailList.push(midweek.checked);
-      console.log(emails);
-      // addEmail();
-      return true;
+    } else if (regex.test(input.value) == true) {
+        feedback.success();
+        emailList.push(input.value);
+        emailList.push(midweek.checked);
+        console.log(emailList);
+        return true;
     } else {
-      feedback.invalid();
-      feedback.successRemove();
-      return false;
-    }
+        feedback.invalid();
+        feedback.successRemove();
+        return false;
+      }
   });
 }
 
-let emailList = [];
 
 
+let feedback = {
+  alertFeedback: function(text) {
+    let successMessage = document.querySelector(".success-feedback");
+    let alertMessage = document.querySelector(".alert-feedback");
+    let forms = document.querySelectorAll('.newsletter-form');
+    let formsArray = Array.from(forms);
+    let htmlText = `<div class=\"alert-feedback-container alert-feedback\"><span class=\"material-icons alert-icon\">warning</span><p>${text}<p></div>`;
 
+    formsArray.forEach(form => {
+      if (document.body.contains(successMessage)) {
+        successMessage.remove();
+      }
+      if (document.body.contains(alertMessage)) {
+        alertMessage.remove();
+      }
 
-let parentElement = document.querySelector('body');
-parentElement.addEventListener('click', showFeedback);
+      feedback.insertHtml(form, "beforeend", htmlText);
+      feedback.alertSetTimeout()
+    });
+  },
+  empty: function() {
+    feedback.alertFeedback("Please, fill in this text");
+  },
+  noAt: function() {
+    feedback.alertFeedback("Please, include an @ in the email address");
+  },
+  noDot: function() {
+    feedback.alertFeedback("Please, a dot is missing in the email address");
+  },
+  invalid: function() {
+    feedback.alertFeedback("Please provide a valid email address");
+  },
+  success: function() {
+    let successMessage = document.querySelector(".success-feedback");
+    let alertMessage = document.querySelector(".alert-feedback");
+    let forms = document.querySelectorAll('.newsletter-form');
+    let formsArray = Array.from(forms);
+    let htmlText = "<p class=\"success-feedback\">Thank you for subscribing! you\'ll receive a confirmation email shortly</p>";
+
+    formsArray.forEach(form => {
+      if (document.body.contains(successMessage)) {
+        successMessage.remove();
+      }
+
+      feedback.insertHtml(form, "afterend", htmlText);
+    });
+  },
+  successRemove: function() {
+    let successMessage = document.querySelector(".success-feedback");
+    if (document.body.contains(successMessage)) {
+      successMessage.remove();
+    }
+  },
+  alertRemove: function() {
+    let alertMessage = document.querySelector(".alert-feedback");
+    alertMessage.remove()
+  },
+  alertSetTimeout: function() {
+    setTimeout(feedback.alertRemove, 2800);
+  },
+  insertHtml: function(element, position, html) {
+    element.insertAdjacentHTML(position, html);
+  },
+};
+
